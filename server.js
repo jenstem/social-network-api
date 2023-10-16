ebe183574d02a17ffe9a7492196b62eb4a7f434f
jenstem <124745888+jenstem@users.noreply.github.com>
2023-10-16 17:06:07 -0400

    Add server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+// Dependencies
+const express = require('express');
+const db = require('./config/connection');
+const mongoose = require('mongoose');
+// app.use(require('./routes'));
+const routes = require('./routes');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+app.use(routes);
+
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+    });
+  });

6082a5972350e44b36df71c5a8de67f91c06dce0
jenstem <124745888+jenstem@users.noreply.github.com>
2023-10-16 17:17:54 -0400

    Add GET req for all users to userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,1 +1,23 @@
+const { User } = require("../models");
 
+const userController = {
+
+  // get all users
+  async getUsers(req, res) {
+    try {
+      const userData = await User.find()
+        .select("-__v")
+        .populate('thoughts');
+      res.json(userData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+
+
+
+
+  }
+
+module.exports = userController;

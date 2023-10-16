8f06be09a047c8b02fef3709c2a8efce23b73f6b
jenstem <124745888+jenstem@users.noreply.github.com>
2023-10-16 17:15:34 -0400

    Add userRoutes.js to api folder

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  removeUser,
+  addFriend,
+  removeFriend,
+} = require('../../controllers/userController');
+
+// /api/users
+router.route('/').get(getUsers).post(createUser);
+
+// /api/users/:userId
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(removeUser);
+
+// /api/users/:userId/friends
+router.route('/:userId/friends').post(addFriend);
+
+// /api/users/:userId/friends/:friendId
+router.route("/:userId/friends/:friendId").post(addFriend).delete(removeFriend);
+
+
+module.exports = router;
